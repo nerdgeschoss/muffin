4d689a5d21624159dad7cd5056f6f84ca7d96f37
Michael Sievers <msievers@users.noreply.github.com>
2017-07-11 11:15:08 +0200

    Updated spec_helper according to latest bundler

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,6 +19,16 @@ if RUBY_ENGINE == "ruby"
 end
 
 RSpec.configure do |config|
+  # Enable flags like --only-failures and --next-failure
+  config.example_status_persistence_file_path = ".rspec_status"
+
+  # Disable RSpec exposing methods globally on `Module` and `main`
+  config.disable_monkey_patching!
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
